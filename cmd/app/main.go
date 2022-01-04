ef9c4260f1169019dc2a2ee3b3ed5c3ac96b0a36
Reshi E520 <reshimahendra@gmail.com>
2022-01-04 20:10:07 +0800

    Remove main.go file to 'cmd/app'

---
+++ cmd/app/main.go
@@ -0,0 +1,10 @@
+/*
+    Main Package is use to call function from 'Server.go' file
+*/
+package main
+
+import "github.com/reshimahendra/gin-starter/cmd/app/server"
+
+func main() {
+    server.Run()
+}
