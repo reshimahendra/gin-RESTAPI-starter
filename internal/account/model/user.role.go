88648f170f518e738b70f8eeb44c11f8db0c40d6
Reshi E520 <reshimahendra@gmail.com>
2022-01-06 00:17:57 +0800

    relocate and refactoring code for user and user.role

---
+++ internal/account/model/user.role.go
@@ -0,0 +1,15 @@
+/*
+    Package model for 'Role'
+*/
+package model
+
+// User Role
+type Role struct {
+    ID          uint    `gorm:"primaryKey;" json:"id"`
+    Name        string  `gorm:"type:varchar(25);unique;" json:"name"`
+    Description string  `json:"description"`
+}
+
+func (r *Role) TableName() string {
+    return "Role"
+}
